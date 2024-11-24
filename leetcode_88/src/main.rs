fd9c611b7dce48d6a813b2474e2f68f164fdb232
mingchiuli <mingchiuli@163.com>
2024-11-24 23:18:39 +0800

    remove test

--- leetcode_88/src/main.rs
+++ leetcode_88/src/main.rs
@@ -3,11 +3,6 @@
 
 fn main() {
     let mut nums1 = vec!(0,0,3,0,0,0,0,0,0);
-    
-    for i in &nums1 {
-        print!("{}", i)
-    }
-    
     let mut nums2 = vec!(-1,1,1,1,2,3);
     Solution::merge(&mut nums1, 3, &mut nums2, 6);
     print!("{:?}", nums1); 
